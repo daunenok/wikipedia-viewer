d357710447a2e7bf4f7fe57b8b3db83b8a3ae5f0
daunenok <daunenok@list.ru>
2017-05-07 20:31:06 +0600

    Add main content

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$("#search").on("click", function(event) {
+		event.preventDefault();
+		var str = "http://en.wikipedia.org/w/api.php?" + 
+		          "action=query&generator=search&gsrsearch=" + 
+		          $("#text-search").val() + 
+		          "&format=json&prop=info&" + 
+		          "inprop=url|displaytitle&origin=*";
+		$.get(str, showResults);
+	});
+});
+
+function showResults(data) {
+	var pages = data.query.pages;
+	var element;
+	$("#results").empty();
+
+	for (item in pages) {
+		element = "<div class='panel panel-primary'>";
+		element += "<div class='panel-body'>";
+		element += "<a href='" + pages[item].fullurl;
+		element += "' target='_blank'>";
+		element += pages[item].displaytitle;
+		element += "</a></div></div>";
+		$("#results").append(element);
+	}
+}
+   
